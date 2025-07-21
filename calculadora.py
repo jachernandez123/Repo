1bb2e12c09d4759e07edccc028af71eb4d110c09
Carlos Hernandez <carloskant@hotmail.com>
2025-07-21 11:19:55 -0500

    cambio nombre

--- calculadora.py
+++ calculadora.py
@@ -15,7 +15,7 @@ def dividir(a, b):
     else:
         return "Error: División por cero"
 
-print("Calculadora básica 21 de julio OJO RAMA 3")
+print("Calculadora básica 21 de julio primera carga")
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
